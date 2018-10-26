e1005e13a22b95f90624349a1f413d975d4dabad
WebmasterTD <webmastertd000@gmail.com>
2018-10-26 17:51:58 +0200

    added wifi config

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -1,12 +1,24 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+#import webrepl
+#webrepl.start()
+
+########################
+# Wifi passwords here: #
+
+########################
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('<essid>', '<password>')
+        sta_if.connect(WIFI_SSID, WIFI_PASSWORD)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
 
+gc.collect()
 do_connect()
--- esp8266/main.py
+++ esp8266/main.py
@@ -1,23 +1,41 @@
 from encoder import Encoder
 from utime import sleep_ms
-import network
+import usocket, ussl
 
 #ToDo 
 #1. Connect to WiFi -- in boot.py
+
 #2. Connect with socket to chromecast
 #3. Get current volume
 #4. If encoder value is changing, change volume
 #5. Wait for a few senconds then go to sleep
 
-def main():
-    enc = Encoder(4, 0, clicks=2)
-    lastval = 0
+########################
+# Wifi config here:    #
 
+########################
+
+
+def main():
+    #Initialize the encoder:
+    enc = Encoder(4, 0, clicks=2)
+    last_enc_val = 0
+
+    #Socket connection set-up:
+    s = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s = ussl.wrap_socket(s)
+
+    #Get current voulme:
+    #to be implemented
+    req  = 2
     while True:
         val = enc.value
-        if lastval != val:
-            lastval = val
+        if last_enc_val != val:
+            last_enc_val = val
+            set_volume(val, req)
             print(val)
+        
         sleep_ms(100)
 
 
