df9716910efc8a9baa0421e29dc5361b1fd20935
WebmasterTD <webmastertd000@gmail.com>
2018-12-04 16:11:39 +0100

    removed unnecessary tests

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -2,16 +2,28 @@
 #import esp
 #esp.osdebug(None)
 import gc
+import utime as time
 #import webrepl
 #webrepl.start()
 
 ########################
 # Wifi passwords here: #
-
+WIFI_SSID = 'TP-LINK_E180'
+WIFI_PASSWORD = 'borz52KAI?'
 ########################
+def timed(func):
+    def wrapper():
+        start = start = time.ticks_ms() # get millisecond counter
+        func()
+        delta = time.ticks_diff(time.ticks_ms(), start)
+        print("wifi connection time: ", delta, " ms")
+    return wrapper
 
+@timed
 def do_connect():
     import network
+    #import machine
+
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
@@ -19,6 +31,7 @@ def do_connect():
         sta_if.connect(WIFI_SSID, WIFI_PASSWORD)
         while not sta_if.isconnected():
             pass
+            #machine.idle()
     print('network config:', sta_if.ifconfig())
 
 gc.collect()
